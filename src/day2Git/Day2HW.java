debe45228989e9b365bd5fede6973a44fbed7699
bruka <bruka@LAPTOP-43QTSTF8>
2023-04-10 14:34:51 -0700

    added subtract() and pushing

--- src/day2Git/Day2HW.java
+++ src/day2Git/Day2HW.java
@@ -8,4 +8,16 @@ public class Day2HW {
 		
 		System.out.println("Remote Repository");
 	}
+	
+	//Stash
+	public void add(int a, int b) {
+		
+		System.out.println(a + b);
+	}
+	
+	//Push
+	public void subtract(int a, int b) {
+		
+		System.out.println(a - b);
+	}
 }
